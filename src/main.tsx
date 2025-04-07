82cc071d5132f54801c5eb48d9cb6ab5034b0268
Lei <leipeng950504@gmail.com>
2025-04-07 17:05:17 +0800

    入口恢复

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import App from './App.tsx';
 
 // 初始化 Helux store
 // 注意：如果有需要添加其他插件的话，可以使用 addPlugin 方法
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(
+  (() => {
+    const app = document.createElement('div');
+    document.body.append(app);
+    return app;
+  })(),
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
